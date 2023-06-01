d77e8ef06c373dd1e986c32e7aa86483964ace1a
liuqi <boxingliuqi@163.com>
2023-06-01 13:43:42 +0800

    .

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'webSite-52lkk'
+
 export default defineConfig({
+  title: 'webSite-52lkk',
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   themeConfig: {
     name: '52lkk',
   },
